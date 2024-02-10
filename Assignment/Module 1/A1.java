90fe3dd637b39fffd99d633578fa19a88a3a6038
Manvir3124 <159610476+Manvir3124@users.noreply.github.com>
2024-02-10 13:07:14 +0530

    Module1

---
+++ Assignment/Module 1/A1.java
@@ -0,0 +1,33 @@
+package ASSIGNMENT1;
+
+public class A1 
+package com.oops;
+
+import java.util.Scanner;
+
+public class A1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("enter no 1:  ");
+		int a=sc.nextInt();
+		
+		System.out.println("enter no 2: ");
+		int b=sc.nextInt();
+		
+
+		System.out.println("enter no 3: ");
+		int c=sc.nextInt();
+		
+		if(a)
+		
+		
+		
+		
+	}
+}
+{
+
+}
---
+++ Assignment/Module 1/A5.java
@@ -0,0 +1,5 @@
+package ASSIGNMENT1;
+
+public class A5 {
+
+}
